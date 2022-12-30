88617fec30b3ba1c5735486a687faa4541169022
Jin Hyeok Ahn <ly0526@naver.com>
2022-12-30 17:34:45 +0900

    금요일_웹 구현_진혁
    로그인 페이지 형성

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,17 @@
+from .models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email = validated_data['email'],
+            nickname = validated_data['nickname'],
+            name = validated_data['name'],
+            location = validated_data["location"],
+            area = validated_data["area"],
+            password = validated_data['password'],
+        )
+        return user
+    class Meta:
+        model = User
+        fields = ['nickname', 'email', 'name', "location", "area", 'password']
--- backend/djangoreactapi/urls.py
+++ backend/djangoreactapi/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/accounts/v1/", include("accounts.urls")),
+    path("user/", include("accounts.urls")),
 ]
