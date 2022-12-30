f0634ded05012e908656fcf189d1ec1a2dc4075a
Jin Hyeok Ahn <ly0526@naver.com>
2022-12-30 18:06:16 +0900

    올해 마지막 커밋
    드롭다운 추각

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -14,6 +14,7 @@ class UserManager(BaseUserManager):
             raise ValueError('must have user location')
         if not area:
             raise ValueError('must have user area')
+
         user = self.model(
             email = self.normalize_email(email),
             nickname = nickname,
@@ -38,11 +39,32 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser):
+
+    location_choice = (
+            ("Seoul", "서울특별시"),
+            ("Busan", "부산광역시"),
+            ("Daegu", "대구광역시"),
+            ("Incheon", "인천광역시"),
+            ("Gwangju", "광주광역시"),
+            ("Daejeon", "대전광역시"),
+            ("Ulsan", "울산광역시"),
+            ("Sejong", "세종특별자치시"),
+            ("Gyeonggi", "경기도"), 
+            ("Gwangwon", "강원도"),
+            ("Chungcheong_north", "충청북도"),
+            ("Chungcheong_south", "충청남도"),
+            ("Jeolla_north", "전라북도"),
+            ("Jeolla_south", "전라남도"),
+            ("Gyeongsang_north", "경상북도"),
+            ("Gyeongsang_south", "경상남도"),
+            ("Jeju", "제주도"),
+        )
+
     id = models.AutoField(primary_key=True)
     email = models.EmailField(default='', max_length=100, null=False, blank=False, unique=True)
     nickname = models.CharField(default='', max_length=100, null=False, blank=False, unique=True)
     name = models.CharField(default='', max_length=100, null=False, blank=False)
-    location = models.CharField(default="서울", max_length=100, null=False, blank=False, unique=True)
+    location = models.CharField(default="서울특별시", max_length=100, null=False, blank=False, unique=True, choices=location_choice)
     area = models.FloatField(default=0, max_length=50, null=False, blank=False, unique=False)
     
     # User 모델의 필수 field
