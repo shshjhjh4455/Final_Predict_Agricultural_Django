d9aaa503487fb7b8269c35cbacfff8dceed674e7
username <111476406+Kim-Hyunah@users.noreply.github.com>
2022-12-30 18:05:03 +0900

    지역 추가

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -37,12 +37,33 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+location = (
+    ("서울", "서울"),
+    ("경기", "경기"),
+    ("인천", "인천"),
+    ("강원", "강원"),
+    ("충북", "충북"),
+    ("충남", "충남"),
+    ("대전", "대전"),
+    ("세종", "세종"),
+    ("전북", "전북"),
+    ("전남", "전남"),
+    ("광주", "광주"),
+    ("대구", "대구"),
+    ("경북", "경북"),
+    ("경남", "경남"),
+    ("부산", "부산"),
+    ("울산", "울산"),
+    ("제주", "제주"),
+    ("해외", "해외"),
+)
+
 class User(AbstractBaseUser):
     id = models.AutoField(primary_key=True)
     email = models.EmailField(default='', max_length=100, null=False, blank=False, unique=True)
     nickname = models.CharField(default='', max_length=100, null=False, blank=False, unique=True)
     name = models.CharField(default='', max_length=100, null=False, blank=False)
-    location = models.CharField(default="서울", max_length=100, null=False, blank=False, unique=True)
+    location = models.CharField(default="서울", max_length=100, null=False, blank=False, unique=True, choices=location)
     area = models.FloatField(default=0, max_length=50, null=False, blank=False, unique=False)
     
     # User 모델의 필수 field
