938d9cdf749c45e42ba607bafe7873580b4544a5
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-01-02 14:05:48 +0900

    make components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,22 @@
-import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
+import React, {Suspense} from 'react';
+import './App.css';
+import NavBar from './components/NavBar/NavBar';	// 추가
+import LoginPage from './components/UserPage/LoginPage';	// 추가
+import SignupPage from './components/UserPage/SignupPage';	// 추가
 
-class App extends Component {
-  state = {
-    accounts: []
-  };
-
-  async componentDidMount() {
-    try {
-      const res = await fetch('http://127.0.0.1:8000/user/signup');
-      const accounts = await res.json();
-      this.setState({
-        accounts
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.accounts.map(item => (
-          <div key={item.name}>
-            <h1>{item.email}</h1>
-            <span>{item.name}</span>
-          </div>
-        ))}
-      </div>
+function App() {
+  return (
+    <Suspense fallback={(<div>...</div>)}>
+      <NavBar />
+      <div className="App">
+          <Switch>
+            <Route exact path="/login" component={LoginPage}></Route>	
+            <Route exact path="/signup" component={SignupPage}></Route>	
+          </Switch>
+        </div>
+    </Suspense>
     );
-  }
 }
 
 export default App;
