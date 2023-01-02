857a9be373b33e7ed0acd2d4eac3af488aaca14f
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-01-02 11:42:23 +0900

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("signup/", views.UserCreate.as_view()),
     path("api-auth/", include("rest_framework.urls")),
     # login 버튼 누른 후 url로 이동
-    path("login/<int:pk>/", views.LoginView.as_view()),
+    path("login/", views.LoginView.as_view()),
 ]
