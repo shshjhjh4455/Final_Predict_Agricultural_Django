2f2d123c3773e768e1fe399e2103c142d55cd675
username <111476406+Kim-Hyunah@users.noreply.github.com>
2023-01-03 13:12:20 +0900

    마이페이지

--- common/forms.py
+++ common/forms.py
@@ -5,7 +5,11 @@ from django.contrib.auth.models import User
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
+    location = forms.CharField(label="지역")
+    area = forms.CharField(label="재배면적")
+    phone = forms.CharField(label="전화번호")
+
 
     class Meta:
         model = User
-        fields = ("username", "password1", "password2", "email")
+        fields = ("username", "password1", "password2", "email", "location", "area", "phone")
