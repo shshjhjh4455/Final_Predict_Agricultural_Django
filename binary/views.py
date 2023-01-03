c08154d58ecd1bf9ebeffee41865d37fbed6a315
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-01-03 14:19:54 +0900

    model 생성

--- binary/views.py
+++ binary/views.py
@@ -5,18 +5,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-# class Prediction(APIView):
-#     def post(self, request):
-#         #data = request.data
-#         age= request.GET.get('age')
-#         gender = request.GET.get('gender')
-#         bp = request.GET.get('bp')
-#         cholesterol = request.GET.get('cholesterol')
-#         salt = request.GET.get('salt')
-#         dtree = ApiConfig.model
-#         #predict using independent variables
-#         PredictionMade = dtree.predict([[age, gender, cholesterol, bp, salt]])
-#         response_dict = {"Predicted drug": PredictionMade}
-#         print(response_dict)
-#         return Response(response_dict, status=200)
+class Prediction(APIView):
+    def post(self, request):
+        #data = request.data
+        # 사용자가 입력한 데이터를 받아옴, location은 사용자가 입력한 값
+        location = request.data['location']
+        age= request.GET.get('age')
+        gender = request.GET.get('gender')
+        bp = request.GET.get('bp')
+        cholesterol = request.GET.get('cholesterol')
+        salt = request.GET.get('salt')
+        dtree = ApiConfig.model
+        #predict using independent variables
+        PredictionMade = dtree.predict([[age, gender, cholesterol, bp, salt]])
+        response_dict = {"Predicted drug": PredictionMade}
+        print(response_dict)
+        return Response(response_dict, status=200)
 
