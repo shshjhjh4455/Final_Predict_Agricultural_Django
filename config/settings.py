1cd23e8dd07725add70bb8964bb23a164e99d3ac
username <111476406+Kim-Hyunah@users.noreply.github.com>
2023-01-03 15:52:34 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -43,9 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "config.settings",
-    "rest_framework",
-    "corsheaders",
 ]
 
 MIDDLEWARE = [
