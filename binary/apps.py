249aca7d1dcbc0041cf3640e5d7b237d7c10553b
username <111476406+Kim-Hyunah@users.noreply.github.com>
2023-01-03 14:21:04 +0900

    딥러닝 모델 코드 수정

--- binary/apps.py
+++ binary/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from deep.model import DeepModel
+import os
+import joblib
+from django.conf import settings
 
-
-class BinaryConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "binary"
+class ApiConfig(AppConfig):
+    name = 'api'
+    MODEL_FILE = os.path.join(settings.MODEL, "xgb_baechoo_bin_classify_jinhyeok.pickle")
+    model = joblib.load(MODEL_FILE)
--- binary/views.py
+++ binary/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import numpy as np
+import pandas as pd
+from .apps import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class Prediction(APIView):
+    def post(self, request):
+        #data = request.data
+        #작년 데이터 불러오기
+        age= request.GET.get('age')
+        gender = request.GET.get('gender')
+        bp = request.GET.get('bp')
+        cholesterol = request.GET.get('cholesterol')
+        salt = request.GET.get('salt')
+        dtree = ApiConfig.model
+        #predict using independent variables
+        PredictionMade = dtree.predict([[age, gender, cholesterol, bp, salt]])
+        response_dict = {"Predicted drug": PredictionMade}
+        print(response_dict)
+        return Response(response_dict, status=200)
