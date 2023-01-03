4b2add07fbac53a7e861e8e3a3744c2a8e1dcaae
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-01-03 11:13:55 +0900

    초기화

--- common/forms.py
+++ common/forms.py
@@ -5,18 +5,7 @@ from django.contrib.auth.models import User
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
-    area = forms.CharField(label="지역")
-    phone = forms.CharField(label="전화번호")
-    location = forms.CharField(label="위치")
 
     class Meta:
         model = User
-        fields = (
-            "username",
-            "password1",
-            "password2",
-            "email",
-            "area",
-            "phone",
-            "location",
-        )
+        fields = ("username", "password1", "password2", "email")
