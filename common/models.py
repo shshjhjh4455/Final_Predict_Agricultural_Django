ee20a72df625cc0ff3f20673ce150aeccacea27d
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-01-05 10:07:05 +0900

    추천 페이지 업데이트

--- common/models.py
+++ common/models.py
@@ -9,6 +9,7 @@ class UserInfo(models.Model):
     phone = models.CharField(max_length=20, null=True)
     area = models.CharField(max_length=20, null=True)
     location = models.CharField(max_length=20, null=True)
+    month = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.user.username
--- recommend/views.py
+++ recommend/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pickle
 import joblib
+from common.models import UserInfo
+from django.contrib.auth.decorators import login_required
 
 
 def predict(request):
@@ -32,7 +34,15 @@ def predict(request):
         model = pickle.load(f)
         y_p = model.predict(feature)
 
+    user = request.user
+    user_info = UserInfo.objects.get(user=user)
+
     if y_p == 1:
-        return render(request, "common/recommend.html", {"y_p": "배추 농사 재배가 가능합니다."})
+        # y_p, user_info를 recommend.html에 넘겨줌
+        return render(
+            request,
+            "common/recommend.html",
+            {"y_p": "배추 농사 재배가 가능합니다.", "user_info": user_info},
+        )
     else:
         return render(request, "common/recommend.html", {"y_p": "배추 농사 재배가 불가능합니다."})
