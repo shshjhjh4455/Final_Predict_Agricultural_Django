d4fd94c1a214ed625a06f32866b059b996744e2b
hyunA <111476406+Kim-Hyunah@users.noreply.github.com>
2023-01-10 17:54:30 +0900

    api 코드 추가

--- api_checker/api.py
+++ api_checker/api.py
@@ -3,6 +3,12 @@ import json
 import pprint
 from urllib.parse import urlencode, unquote, quote_plus
 from bs4 import BeautifulSoup
+from urllib.request import urlopen, Request
+import xml.etree.ElementTree as ET
+import pandas as pd
+import numpy as np
+import datetime
+
 
 
 serviceKey = "c51eace9-59a8-4c45-921f-ad68c3507a7b"
@@ -12,60 +18,57 @@ serviceKeyDecoded = unquote(serviceKey, 'UTF-8')
     &p_endday=2015-12-01&p_itemcategorycode=200&p_itemcode=212&p_kindcode=00&p_productrankcode=04&p_countrycode=1101
     &p_convert_kg_yn=Y&p_cert_key=111&p_cert_id=222&p_returntype=xml"""
 
-
-
-# def check_api():
-#     itemname = []
-#     price = []
-#     url = "http://www.kamis.or.kr/service/price/xml.do?action=periodProductList"
-#     p_productclscode="02"
-#     p_startday="2022-01-20"
-#     p_endday="2023-01-05"
-#     p_itemcategorycode="200"
-#     p_itemcode="211"
-#     p_kindcode="00"
-#     p_productrankcode="04"
-#     p_countrycode="default"
-#     p_convert_kg_yn="default"
-
-
-    # queryParams = '?' + urlencode({ quote_plus('ServiceKey') : serviceKeyDecoded, quote_plus('p_productclscode') : p_productclscode, quote_plus('p_startday') : p_startday, quote_plus('p_endday') : p_endday, quote_plus('p_itemcategorycode') : p_itemcategorycode, quote_plus('p_itemcode') : p_itemcode, quote_plus('p_kindcode') : p_kindcode, quote_plus('p_productrankcode') : p_productrankcode, quote_plus('p_countrycode') : p_countrycode, quote_plus('p_convert_kg_yn') : p_convert_kg_yn })
-    # res = requests.get(url + queryParams)
-    # xml = res.text
-    # soup = BeautifulSoup(xml, 'html.parser')
-    # for tag in soup.find_all('itemname'):
-    #     itemname.append(tag.text)
-    # for tag in soup.find_all('price'):
-    #     price.append(tag.text)
-    # res = dict(zip(itemname, price))
-    # return res
-
 def check_api():
-    itemname = []
-    price = []
-    url = "http://www.kamis.or.kr/service/price/xml.do?action=periodProductList"
+    url = "http://www.kamis.or.kr/service/price/xml.do"
     returnType="json"
     ID = 3067
     p_itemcategorycode = 200
     p_itemcode = 211
-    start_date = "2022-01-01"
-    end_date = "2023-01-10"
+    today= datetime.datetime.today()
+    start_date = (today - datetime.timedelta(365)).strftime('%Y-%m-%d')
+    end_date = today.strftime('%Y-%m-%d')
+    p_productrankcodes = ["04", "05"]
+    
+    for rank in p_productrankcodes :
+    
+        queryParams = '?' + urlencode({ quote_plus("action") : "periodProductList",
+                                       quote_plus("p_startday") : start_date, 
+                                       quote_plus("p_endday") : end_date,
+                                       quote_plus("p_itemcategorycode") : p_itemcategorycode, 
+                                       quote_plus("p_itemcode") : p_itemcode, 
+                                       quote_plus("p_productrankcode") : rank, 
+                                       quote_plus('p_cert_key') : serviceKeyDecoded, 
+                                       quote_plus('p_cert_id') : ID, quote_plus('p_returntype') : returnType })
+        res = requests.get(url + queryParams)
 
-    queryParams = '?' + urlencode({ 
-                                   quote_plus("p_startday") : start_date, 
-                                   quote_plus("p_endday") : end_date,
-                                   quote_plus("p_itemcategorycode") : p_itemcategorycode, 
-                                   quote_plus("p_itemcode") : p_itemcode, 
-                                   quote_plus('p_cert_key') : serviceKeyDecoded, 
-                                   quote_plus('p_cert_id') : ID, quote_plus('p_returntype') : returnType })
+        contents = res.text
+        json_ob = json.loads(contents)
+        choice = json_ob["data"]["item"]
 
+        year = []
+        date = []
+        price = []
+        for dct in choice : 
+            if dct["countyname"] == "평균" : 
+                year.append(dct["yyyy"])
+                date.append(dct["regday"])    
+                price.append(dct["price"])
 
-    res = requests.get(url + queryParams)
-    xml = res.text
-    soup = BeautifulSoup(xml, 'html.parser')
-    for tag in soup.find_all('itemname'):
-        itemname.append(tag.text)
-    for tag in soup.find_all('price'):
-        price.append(tag.text)
-    res = dict(zip(itemname, price))
-    return res
+        globals()["df_"+rank] = pd.DataFrame(zip(year,date,price))
+        globals()["df_"+rank].columns = ["year", "date", "price_"+rank]
+        
+    df = pd.merge(df_04, df_05, on = ["year","date"], how="inner")
+    df["price_04"] = df["price_04"].str.replace(",","").astype("int")
+    df["price_05"] = df["price_05"].str.replace(",","").astype("int")
+
+    df["가격"] = df[["price_04", "price_05"]].mean(axis="columns").astype("int")
+    df.drop(columns=["price_04", "price_05"], inplace=True)
+    
+    cols = ['year', 'date']
+    df['날짜'] =df[cols].apply(lambda row: '/'.join(row.values.astype(str)), axis=1)
+    df['날짜']= pd.to_datetime(df['날짜'])
+    df= df.drop(labels=['year', 'date'], axis=1)
+    
+    df= df.set_index('날짜')
+    
+    return df
