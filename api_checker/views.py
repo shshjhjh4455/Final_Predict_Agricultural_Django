2f60d3d4589f29e77471f5acc658cb34df60c2c5
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-01-11 10:59:44 +0900

    predict model

--- api_checker/views.py
+++ api_checker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .api import check_api
+from .api import get_candle_df
 import json
 import pandas as pd
 import pickle
@@ -13,9 +13,9 @@ import datetime
 #     year = []
 #     date = []
 #     price = []
-#     for dct in choice : 
+#     for dct in choice :
 #         year.append(dct["yyyy"])
-#         date.append(dct["regday"])    
+#         date.append(dct["regday"])
 #         price.append(dct["price"])
 
 #     context = {'year': '2022', 'price': price}
@@ -36,45 +36,63 @@ import datetime
 #     context = {'dust': res}
 #     return render(request, 'common/api_detail.html', context)
 
+
 # 예측하는 페이지 전에 보여주는 페이지.
 def predict_price(days):
-  # Load the model and scalers
-  model = pickle.load(open('model/price_candle_XGBoost_continuous_{}days.pkl'.format(days), 'rb'))
-  scaler1 = pickle.load(open('model/price_candle_scaler1_{}.pkl'.format(days), 'rb'))
-  scaler2 = pickle.load(open('model/price_candle_scaler2_{}.pkl'.format(days), 'rb'))
 
-  # Select the last `days` days of data
-  candle_df_last_x = candle_df_lasts[days]
+    # Load the model and scalers
+    model = pickle.load(
+        open("model/price_candle_XGBoost_continuous_{}days.pkl".format(days), "rb")
+    )
+    scaler1 = pickle.load(open("model/price_candle_scaler1_{}.pkl".format(days), "rb"))
+    scaler2 = pickle.load(open("model/price_candle_scaler2_{}.pkl".format(days), "rb"))
 
-  # Drop the '종가_shift' column
-  candle_df_last_x = candle_df_last_x.drop(f'{days}종가_shift')
+    # load df
+    candle_df_lasts = get_candle_df()
 
-  # Convert candle_df_last_x to a Pandas dataframe
-  candle_df_last_x = pd.DataFrame(candle_df_last_x)
-  candle_df_last_x= candle_df_last_x.T
+    # Select the last `days` days of data
+    candle_df_last_x = candle_df_lasts[days]
 
-  # Model predict using test_data
-  test_data_sc = scaler1.transform(candle_df_last_x)
-  test_data_sc = pd.DataFrame(test_data_sc, columns=candle_df_last_x.columns, index=candle_df_last_x.index)
-  y_pred = model.predict(test_data_sc)
+    # Drop the '종가_shift' column
+    candle_df_last_x = candle_df_last_x.drop(f"{days}종가_shift")
 
-  # reshape y_pred
-  y_pred = y_pred.reshape(-1,1)
+    # Convert candle_df_last_x to a Pandas dataframe
+    candle_df_last_x = pd.DataFrame(candle_df_last_x)
+    candle_df_last_x = candle_df_last_x.T
 
-  # Inverse transform y_pred
-  y_pred = scaler2.inverse_transform(y_pred)
+    # Model predict using test_data
+    test_data_sc = scaler1.transform(candle_df_last_x)
+    test_data_sc = pd.DataFrame(
+        test_data_sc, columns=candle_df_last_x.columns, index=candle_df_last_x.index
+    )
+    y_pred = model.predict(test_data_sc)
 
-  # Return the predicted values
-  return y_pred
+    # reshape y_pred
+    y_pred = y_pred.reshape(-1, 1)
+
+    # Inverse transform y_pred
+    y_pred = scaler2.inverse_transform(y_pred)
+
+    # Return the predicted values
+    return y_pred
+
 
 def index(request):
-    res = check_api()
-    return render(request, 'common/predict.html')
+    return render(request, "common/predict.html")
+
 
 def detail(request):
-    res = check_api()
-    # res를 dict로 바꿔서 context에 넣어준다.
-    context = {'res': res}
-    return render(request, 'common/api_detail.html', context)
+    pred_5 = predict_price(5)
+    pred_10 = predict_price(10)
+    pred_20 = predict_price(20)
+    pred_60 = predict_price(60)
+    pred_120 = predict_price(120)
 
-
+    context = {
+        "pred_5": pred_5,
+        "pred_10": pred_10,
+        "pred_20": pred_20,
+        "pred_60": pred_60,
+        "pred_120": pred_120,
+    }
+    return render(request, "common/api_detail.html", context)
