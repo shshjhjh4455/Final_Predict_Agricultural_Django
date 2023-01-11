92c3390d7e0a1518c883f9de4e9f19a4e2158191
hyunA <111476406+Kim-Hyunah@users.noreply.github.com>
2023-01-11 17:25:29 +0900

    원상복귀

--- api_checker/views.py
+++ api_checker/views.py
@@ -8,6 +8,8 @@ import datetime
 from django.shortcuts import render
 from .models import Result
 from datetime import date
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 # 예측하는 페이지 전에 보여주는 페이지.
 def predict_price(days):
@@ -48,68 +50,19 @@ def predict_price(days):
     # Return the predicted values
     return y_pred
 
-
 def index(request):
     return render(request, "common/predict.html")
 
-# def detail(request):
-#     date_string = value.strftime("%Y-%m-%d")
-#     date_object = datetime.date.fromisoformat(date_string)
-#     if not Result.objects.filter(date=date_object).exists():
-#         pred_5 = predict_price(5)
-#         pred_10 = predict_price(10)
-#         pred_20 = predict_price(20)
-#         pred_60 = predict_price(60)
-#         pred_120 = predict_price(120)
-
-#         context = Result.objects.create(date=today,pred_5=pred_5, pred_10=pred_10, pred_20=pred_20, pred_60=pred_60, pred_120=pred_120)
-#     else:
-#         context = Result.objects.filter(date=today).first()
-#     return render(request, 'common/api_detail.html', {'context': context})
-
-# def detail(request):
-#     date_string = value
-#     date_object = datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
-#     if not Result.objects.filter(date=date_object).exists():
-#         pred_5 = predict_price(5)
-#         pred_10 = predict_price(10)
-#         pred_20 = predict_price(20)
-#         pred_60 = predict_price(60)
-#         pred_120 = predict_price(120)
-
-#         context = Result.objects.create(date=date_object,pred_5=pred_5, pred_10=pred_10, pred_20=pred_20, pred_60=pred_60, pred_120=pred_120)
-#     else:
-#         context = Result.objects.filter
-
-
-# def detail(request):
-#     # date 다름 또는 시간이 4시를 넘거나 또는 생성된 객체가 없거나
-#     #today = datetime.datetime.now()
-#     #today_str= today.strftime('%Y-%m-%d')
-#     #obj= Result.objects.get(pk=1)
-
-#     # if not Result.objects.first().exists() or today_str != obj.date:
-#     #     pred_5 = predict_price(5)
-#     #     pred_10 = predict_price(10)
-#     #     pred_20 = predict_price(20)
-#     #     pred_60 = predict_price(60)
-#     #     pred_120 = predict_price(120)
-
-#     #     context = Result.objects.create(date=today_str,pred_5=pred_5, pred_10=pred_10, pred_20=pred_20, pred_60=pred_60, pred_120=pred_120)
-#     # else:
-#     #     context = Result.objects.filter(date=today_str).first()
-#     return render(request, 'common/api_detail.html')
 def detail(request):
     today = date.today()
     if not Result.objects.filter(date=today).exists():
-        pred_5 = predict_price(5)
-        pred_10 = predict_price(10)
-        pred_20 = predict_price(20)
-        pred_60 = predict_price(60)
-        pred_120 = predict_price(120)
+        pred_5 = int(predict_price(5)[0])
+        pred_10 = int(predict_price(10)[0])
+        pred_20 = int(predict_price(20)[0])
+        pred_60 = int(predict_price(60)[0])
+        pred_120 = int(predict_price(120)[0])
 
         context = Result.objects.create(date=today,pred_5=pred_5, pred_10=pred_10, pred_20=pred_20, pred_60=pred_60, pred_120=pred_120)
     else:
         context = Result.objects.filter(date=today).first()
     return render(request, 'common/api_detail.html', {'context': context})
-
