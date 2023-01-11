87d06122f732a13b32f76b9a187b0503d42c22a7
Jin Hyeok Ahn <ly0526@naver.com>
2023-01-11 11:00:57 +0900

    Update views.py

--- api_checker/views.py
+++ api_checker/views.py
@@ -1,80 +1,68 @@
 from django.shortcuts import render
-from .api import check_api
+from .api import check_api, create_candles, get_candle_df
 import json
 import pandas as pd
 import pickle
 import numpy as np
 import datetime
 
-# def index(request):
-#     res = check_api()
-#     json_ob = json.loads(res)
-#     choice = json_ob["data"]["item"]
-#     year = []
-#     date = []
-#     price = []
-#     for dct in choice : 
-#         year.append(dct["yyyy"])
-#         date.append(dct["regday"])    
-#         price.append(dct["price"])
-
-#     context = {'year': '2022', 'price': price}
-#     return render(request, 'common/api_checker.html', context)
-
-# def detail(request):
-#     res = check_api()
-#     context = {'dust': res}
-#     return render(request, 'common/api_detail.html', context)
-
-# def index(request):
-#     res = check_api()
-#     context = {'dust': res}
-#     return render(request, 'common/api_checker.html', context)
-
-# def detail(request):
-#     res = check_api()
-#     context = {'dust': res}
-#     return render(request, 'common/api_detail.html', context)
-
 # 예측하는 페이지 전에 보여주는 페이지.
-def predict_price(days):
+@login_required(login_url="common:login")
+def predict_price(request, days):
   # Load the model and scalers
-  model = pickle.load(open('model/price_candle_XGBoost_continuous_{}days.pkl'.format(days), 'rb'))
-  scaler1 = pickle.load(open('model/price_candle_scaler1_{}.pkl'.format(days), 'rb'))
-  scaler2 = pickle.load(open('model/price_candle_scaler2_{}.pkl'.format(days), 'rb'))
 
-  # Select the last `days` days of data
-  candle_df_last_x = candle_df_lasts[days]
+    if request.method == "POST":
+        model = pickle.load(open('model/price_candle_XGBoost_continuous_{}days.pkl'.format(days), 'rb'))
+        scaler1 = pickle.load(open('model/price_candle_scaler1_{}.pkl'.format(days), 'rb'))
+        scaler2 = pickle.load(open('model/price_candle_scaler2_{}.pkl'.format(days), 'rb'))
 
-  # Drop the '종가_shift' column
-  candle_df_last_x = candle_df_last_x.drop(f'{days}종가_shift')
+        candle_df_last_x = get_candle_df()
 
-  # Convert candle_df_last_x to a Pandas dataframe
-  candle_df_last_x = pd.DataFrame(candle_df_last_x)
-  candle_df_last_x= candle_df_last_x.T
+        # Select the last `days` days of data
+        candle_df_last_x = candle_df_lasts[days]
 
-  # Model predict using test_data
-  test_data_sc = scaler1.transform(candle_df_last_x)
-  test_data_sc = pd.DataFrame(test_data_sc, columns=candle_df_last_x.columns, index=candle_df_last_x.index)
-  y_pred = model.predict(test_data_sc)
+        # Drop the '종가_shift' column
+        candle_df_last_x = candle_df_last_x.drop(f'{days}종가_shift')
 
-  # reshape y_pred
-  y_pred = y_pred.reshape(-1,1)
+        # Convert candle_df_last_x to a Pandas dataframe
+        candle_df_last_x = pd.DataFrame(candle_df_last_x)
+        candle_df_last_x= candle_df_last_x.T
 
-  # Inverse transform y_pred
-  y_pred = scaler2.inverse_transform(y_pred)
+        # Model predict using test_data
+        test_data_sc = scaler1.transform(candle_df_last_x)
+        test_data_sc = pd.DataFrame(test_data_sc, columns=candle_df_last_x.columns, index=candle_df_last_x.index)
+        y_pred = model.predict(test_data_sc)
 
-  # Return the predicted values
-  return y_pred
+        # reshape y_pred
+        y_pred = y_pred.reshape(-1,1)
 
-def index(request):
-    res = check_api()
-    return render(request, 'common/predict.html')
+        # Inverse transform y_pred
+        y_pred = scaler2.inverse_transform(y_pred)
 
-def detail(request):
-    res = check_api()
-    # res를 dict로 바꿔서 context에 넣어준다.
-    context = {'res': res}
-    return render(request, 'common/api_detail.html', context)
+        context = {"predict" : y_pred[1]}
+
+        # Return the predicted values
+        return render(request, "")
+
+    else:
+        form = PredictForm()
+        user = request.user
+        user_info = UserInfo.objects.get(user=user)
+        context = {
+            "user_info": user_info,
+            "form": form,
+            "target_pred": None,
+        }
+        return render(request, "common/recommend_op.html", context)
+
+# def index(request):
+#     res = check_api()
+#     return render(request, 'common/predict.html')
+
+# def detail(request):
+#     res = check_api()
+#     # res를 dict로 바꿔서 context에 넣어준다.
+#     context = {'res': res}
+#     return render(request, 'common/api_detail.html', context)
 
 
